8561cad4a8d2152b5a986156c06be1b38d382794
unknown <773569292@qq.ocm>
2021-06-22 18:19:07 +0800

    default

--- KBQA/TER/NER.py
+++ KBQA/TER/NER.py
@@ -1,5 +1,7 @@
 from LAC import LAC
 import openpyxl
+import numpy as np
+import re
 
 # 装载LAC模型
 lac = LAC(mode='lac')
@@ -23,4 +25,19 @@ for cases in list(sh.rows)[1:] :
     query_result.append(lac.run(query))
 
 # 得到NER结果 query_result
-print(query_result)
+#print(query_result)
+
+entity_possible = []
+hmm_n = ['n' 'nr' 'nz' 'ns' 'nt' 'an' 'nw' 'vn']
+for cases in query_result:
+    add_entity = []
+    for i in range(len(cases[0])):
+        if cases[1][i] == 'n' or cases[1][i] == 'nr' or cases[1][i] == 'nz' or cases[1][i] == 'ns' or cases[1][i] == 'nt' or cases[1][i] == 'an' or cases[1][i] == 'nw' or cases[1][i] == 'vn':
+            add_entity.append(cases[0][i])
+    if add_entity == []:
+        add_entity = ['空']
+    # print(add_entity)
+
+    entity_possible.append(add_entity)
+
+print(entity_possible)
