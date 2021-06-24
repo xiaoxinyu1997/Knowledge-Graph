a7505ed6a72ef5e582578471f67d9cf0fc6f0fe8
Saipeng-Li <1124563289@qq.com>
2021-06-24 22:09:53 +0800

    run.py --model bert

--- KBQA/Classify/train_eval.py
+++ KBQA/Classify/train_eval.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from sklearn import metrics
 import time
 from utils import get_time_dif
-from KBQA.Classify.models.optimization import BertAdam
+from models.optimization import BertAdam
 
 
 # 权重初始化，默认xavier
